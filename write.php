345bbbf4d454157b37f33e554cad0266238f9b90
ThomasOXY <74791143+ThomasOXY@users.noreply.github.com>
2022-11-29 01:26:06 -0800

    Zapisování do cords.txt

--- write.php
+++ write.php
@@ -1,9 +1,15 @@
 <?php
+
+$x1 = $_POST['x1'];
+$y1 = $_POST['y1'];
+$x2 = $_POST['x2'];
+$y2 = $_POST['y2'];
+
+
 $myfile = fopen("cords.txt", "w") or die("Unable to open file!");
-$Xcord = "LiborX\n";
-fwrite($myfile, $Xcord);
-$Ycord = "LOk\n";
-fwrite($myfile, $Ycord);
+$txt = $x2;
+fwrite($myfile, $txt);
+$txt = $y2;
+fwrite($myfile, $txt);
 fclose($myfile);
 ?>
-hello
